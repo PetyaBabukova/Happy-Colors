60828f4a9f318db4f14c6f5ca63566b9407b84fe
Petya Babukova <p_babukova@abv.bg>
2022-03-23 16:32:14 +0200

    add cart and checkout

--- routes.js
+++ routes.js
@@ -3,19 +3,39 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home')
+    res.render('home');
 });
 
 router.get('/about', (req, res)=>{
-    res.render('about-us')
+    res.render('about-us');
 });
 
 router.get('/contacts', (req, res)=>{
-    res.render('contacts')
+    res.render('contacts');
 });
 
 router.get('/search', (req, res)=>{
-    res.render('search')
-})
+    res.render('search');
+});
+
+router.get('/dolls', (req, res)=>{
+    res.render('product-list');
+});
+
+router.get('/details', (req, res)=>{
+    res.render('product-details');
+});
+
+router.get('/cart', (req, res)=>{
+    res.render('cart');
+});
+
+router.get('/checkout', (req, res)=>{
+    res.render('checkout');
+});
+
+router.get('/order-success', (req,res)=>{
+    res.render('mail-success');
+});
 
 module.exports = router;
